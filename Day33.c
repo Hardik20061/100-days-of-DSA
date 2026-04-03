7325aa74cacb5f35a1d190405c80b4360e3fbe41
Hardik20061 <shardikg9601@gmail.com>
2026-04-03 13:24:05 +0530

    Add files via upload

---
+++ Day33.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <ctype.h>
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+// Push
+void push(char x) {
+    stack[++top] = x;
+}
+
+// Pop
+char pop() {
+    return stack[top--];
+}
+
+// Peek
+char peek() {
+    return stack[top];
+}
+
+// Precedence
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    int i, j = 0;
+
+    scanf("%s", infix);
+
+    for (i = 0; infix[i] != '\0'; i++) {
+        
+        // Operand
+        if (isalnum(infix[i])) {
+            postfix[j++] = infix[i];
+        }
+        // Left parenthesis
+        else if (infix[i] == '(') {
+            push(infix[i]);
+        }
+        // Right parenthesis
+        else if (infix[i] == ')') {
+            while (top != -1 && peek() != '(') {
+                postfix[j++] = pop();
+            }
+            pop(); // remove '('
+        }
+        // Operator
+        else {
+            while (top != -1 && precedence(peek()) >= precedence(infix[i])) {
+                postfix[j++] = pop();
+            }
+            push(infix[i]);
+        }
+    }
+
+    // Pop remaining operators
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';
+
+    printf("%s", postfix);
+
+    return 0;
+}
